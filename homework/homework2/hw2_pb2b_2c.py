40253c0bf731bb72afa681ed0e2d70d2c8e6c2c6
Jaxon Cione <jaci7052@colorado.edu>
2026-09-05 12:30:59 -0600

    Shwoing relative error vs perterbation

--- homework/homework2/hw2_pb2b_2c.py
+++ homework/homework2/hw2_pb2b_2c.py
@@ -2,24 +2,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-A=.5*np.array([[1,1],[1+(10**-10),1-(10**-10)]]) #array given in prob
-A_TA=A.T@A
+def Condition_number(arr):
+    A=.5*np.array([[1,1],[1+(10**-10),1-(10**-10)]]) #array given in prob
+    A_TA=A.T@A
 
-print(A_TA)
+    print(A_TA)
 
-U,S,V=np.linalg.svd(A) #doing a single value decompoision on A
-print("U",U)
-print("S",S)
-print("V",V) 
+    U,S,V=np.linalg.svd(A) #doing a single value decompoision on A
+    print("U",U)
+    print("S",S)
+    print("V",V) 
 
-K=S[0]/S[1] #grabbing the singular values and calculaing condition number
-print("Condition Number",K)
+    return S[0]/S[1] #grabbing the singular values and calculaing condition number
+    
 
+def relative_error(perterbation):
 #initializing matrix/vectors
-A_inv=np.array([[1-(10**10),10**10],[1+(10**10),-10**10]])
-delta_b=np.array([10**-5,2*10**-5])
-x=np.array([1,1])
+    A_inv=np.array([[1-(10**10),10**10],[1+(10**10),-10**10]])
+    x=np.array([1,1])
+    b_vec=[10**perterbation,10**perterbation]
 
 
-relative_error=np.linalg.norm(A_inv@delta_b)/np.linalg.norm(x)  #taking the relative error
-print(relative_error)
+    return np.linalg.norm(A_inv@b_vec)/np.linalg.norm(x)  #taking the relative error
+    
+
+#showing the relation of delta_b(or pertebation vector) as a function of relative error 
+x=np.linspace(-10,0,10)
+f=[relative_error(k) for k in x]
+plt.plot(x,f, colors='purple')
+plt.xlabel("Perterbation (k): x^k where k∈[-10,0]")
+plt.ylabel("Relative Error")
+plt.title("Relative Error versus pertabation")
+plt.show()
