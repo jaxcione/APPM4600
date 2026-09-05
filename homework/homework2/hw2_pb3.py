c6330ed0fedd51a8ea09fa16f6a06e5368535816
Jaxon Cione <jaci7052@colorado.edu>
2026-09-05 16:10:18 -0600

    part d/e for hw

--- homework/homework2/hw2_pb3.py
+++ homework/homework2/hw2_pb3.py
@@ -10,7 +10,41 @@ def F(x):
 perterbations=np.arange(-.0000001,.0000001,10**-9)
 y=[F(k) for k in perterbations]
 
-for i, val in enumerate(perterbations):
-    print(f"Perturbation {i}: {val}, F(x): {y[i]}")
+#for i, val in enumerate(perterbations):
+    #print(f"Perturbation {i}: {val}, F(x): {y[i]}")
 
 #------------------------------------------------------
+
+#part c-----------------------------------------------
+
+x=9.999999995000000E-10
+print(f"F({x})={F(x)}")
+
+#part d/e-----------------------------------------------
+
+actual=10**-9
+
+
+def Taylor_series(y): #taylor series of e when n=2
+    return y+y**2/2
+
+def relative_error(z):
+    return np.abs(actual-Taylor_series(z))/actual
+
+print(relative_error(x)) #we get 0--> we need two terms in the taylor series to get a better approximation of e^x
+
+sum=0
+for k in range(1,10):
+    count=0
+    error=(10**-16)*actual
+    sum+=(x**k/math.factorial(k))
+    if np.abs(sum-actual)<error:
+        count+=1
+        print(f" k={k}")
+    if count==1:
+        break
+
+
+
+
+    
