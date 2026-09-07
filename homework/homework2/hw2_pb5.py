d1d1a717a24384924baa00bd16f016a51682253a
Jaxon Cione <jaci7052@colorado.edu>
2026-09-06 23:11:15 -0600

    problem 5b

---
+++ homework/homework2/hw2_pb5.py
@@ -0,0 +1,47 @@
+import numpy as np
+import math 
+
+
+e=10**-3
+b=4
+a=1
+
+print(np.log(e/(b-a))/np.log(0.5)) # for problem 5a
+
+#function we are trying to find the root of
+def f(x):
+    return x**3+x-4
+
+def bisection(a,b,eps):
+    counter=0 #storing the num of iterations
+
+
+    if f(a)==0:
+        return a 
+    if f(b)==0:
+        return b 
+    if f(a)*f(b)>0:
+        return None
+    if abs(a-b)/abs(b)<eps:
+        return b
+    
+
+    d=.5*(b+a)
+
+    while abs(d-a)/abs(d)>eps:
+        counter+=1
+        if f(d)*f(a)==0:
+            return d
+
+        if f(d)*f(a)>0:
+            a=d
+            fa=fd
+        else:
+            b=d
+         
+        d=.5*(b+a)
+        fd=f(d)
+
+    return (f"Root:{d}",f"Number of Iterations:{counter}")
+
+print(bisection(a,b,e))
