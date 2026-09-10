0cfbe8c21b43317ab6ab8b70cc64698666bb33b7
Jaxon Cione <jaci7052@colorado.edu>
2026-09-10 09:53:28 -0600

    Using py file from canvas

--- Lab/lab2.py
+++ Lab/lab2.py
@@ -1,18 +1,58 @@
 import numpy as np
 
-alpha=2
-def fixed_point(m,N,alpha,sequence):
-    x=np.zeros((N,1))
-    x[0]=sequence
-    for n in range(len(m-1)):
-        x[n+1]=alpha(x[n])
+def driver():
+    # test functions
+    f1 = lambda x: 1+0.5*np.sin(x)
+    # fixed point is alpha1 = 1.4987....
+    f2 = lambda x: 3+2*np.sin(x)
+    #fixed point is alpha2 = 3.09...
+    Nmax = 100
+    tol = 1e-6
+    # test f1 '''
+    x0 = 0.0
+    [xstar,ier] = fixedpt(f1,x0,tol,Nmax)
+    print('the approximate fixed point is:',xstar)
+    print('f1(xstar):',f1(xstar))
+    print('Error message reads:',ier)
+    #test f2 '''
+    x0 = 0.0
+    [xstar,ier] = fixedpt(f2,x0,tol,Nmax)
+    print('the approximate fixed point is:',xstar)
+    print('f2(xstar):',f2(xstar))
+    print('Error message reads:',ier)
+# define routines
+def fixedpt(f,x0,tol,Nmax):
+    ''' x0 = initial guess'''
+    ''' Nmax = max number of iterations'''
+    ''' tol = stopping tolerance'''
+    count = 0
+    while (count <Nmax):
+        count = count +1
+        x1 = f(x0)
+        if (abs(x1-x0) <tol):
+            xstar = x1
+            ier = 0
+            return [xstar,ier]
+        x0 = x1
+        xstar = x1
+        ier = 1
+        return [xstar, ier]
+driver()
 
-    return x
 
-def fixed(v,N,p):
-    x=fixed_point(N,alpha,v)
-    error=np.abs(x-p)
+N=100
+x=np.zeros((N,1))
+
+p=5 #fixed point
+y=np.linspace(1,10,N)
+
+for i in range(0,N-1):
+    x[i]=y[i]
     
-    q=np.log
+eps_nplus1=x[i+1]-p
+eps_n=x[i]-p
+eps_nm1=x[i-1]-p
+
+alpha=np.log(eps_nplus1/eps_n)/np.log(eps_n/eps_nm1)
 
 
