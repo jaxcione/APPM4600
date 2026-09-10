40b84f923587a943731cf773f6af097c8cc36553
Jaxon Cione <jaci7052@colorado.edu>
2026-09-10 09:31:27 -0600

    prelab changes

--- Lab/lab2.py
+++ Lab/lab2.py
@@ -1,12 +1,18 @@
 import numpy as np
 
-def fixed(m,alpha,sequence):
-    pass
-
-def fixed_point(v,N):
+alpha=2
+def fixed_point(N,alpha,sequence):
     x=np.zeros((N,1))
-    x[0]=v
-    for n in range(len(v)):
-        fixed_point=fixed(x[n])
+    x[0]=sequence
+    for n in range(len(m-1)):
+        x[n+1]=alpha(x[n])
 
     return x
+
+def fixed(v,N,p):
+    x=fixed_point(N,alpha,v)
+    error=np.abs(x-p)
+    
+    q=np.log
+
+
